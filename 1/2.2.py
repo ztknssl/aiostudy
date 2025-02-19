27777b844374db1f0ded6372f5ba27be3dd7db0e
ztknssl <davydov.andrey37@gmail.com>
2025-02-19 22:33:14 +0300

    Dictionary №2 second commit

--- 1/2.2.py
+++ 1/2.2.py
@@ -1,25 +1,33 @@
-try:
-    a = int(input('Введите первое число: '))
-    b = int(input('Введите второе число: '))
-except ValueError:
-    print('Вы ввели не число')
-    exit()
-
-operation_list = ['+', '-', '*', '/']
-operation = input('Введите арифметическую операцию: ')
-
-if operation not in operation_list:
-    print('Вы ввели неверный знак операции')
-    exit()
-else:
+ # Запрашиваем ввод до введения корректных данных
+while True:
     try:
-        if operation == '+':
-            print(f'Результат арифметического действия: {a} + {b} = {a + b}')
-        if operation == '-':
-            print(f'Результат арифметического действия: {a} - {b} = {a - b}')
-        if operation == '*':
-            print(f'Результат арифметического действия: {a} * {b} = {a * b}')
-        if operation == '/':
-            print(f'Результат арифметического действия: {a} / {b} = {a / b}')
-    except ZeroDivisionError:
-        print('Деление на ноль')
+        a = int(input('Введите первое число: '))
+        b = int(input('Введите второе число: '))
+        break
+    except ValueError:
+        print('Вы ввели не число')
+
+# Определяем список операций
+operation_list = ['+', '-', '*', '/']
+
+# Проверка операции
+while True:
+    operation = input('Введите арифметическую операцию: ')
+    if operation not in operation_list:
+        print('Вы ввели неверный знак операции')
+    else:
+        break
+
+try:
+    if operation == '+':
+        print(f'Результат арифметического действия: {a} + {b} = {a + b}')
+    elif operation == '-':
+        print(f'Результат арифметического действия: {a} - {b} = {a - b}')
+    elif operation == '*':
+        print(f'Результат арифметического действия: {a} * {b} = {a * b}')
+    elif operation == '/':
+        print(f'Результат арифметического действия: {a} / {b} = {a / b}')
+except ZeroDivisionError:
+    print('Деление на ноль')
+
+
