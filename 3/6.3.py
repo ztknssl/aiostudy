02c985d6bd723b51faa50d7e715935777cba2e6b
ztknssl <davydov.andrey37@gmail.com>
2025-02-27 22:49:33 +0300

    Gate parsing

---
+++ 3/6.3.py
@@ -0,0 +1,95 @@
+# Попытка сделать middlware для обработки исключений и упорядочить код
+import requests
+
+host = "https://api.gateio.ws"
+prefix = "/api/v4"
+headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+url = '/spot/order_book'
+coins_list = ['BTC_USDT', 'ETH_USDT', 'SOL_USDT']
+
+
+# Декоратор для отлова исключений
+def error_catcher(function):
+    def new_func(*args):
+        try:
+            return function(*args)
+        except requests.RequestException as err:
+            print(f'An error occurred in: {function.__name__}')
+            print(f'Request error: {err}')
+        except Exception as err:
+            print(f'An error occurred in: {function.__name__}')
+            print(f'Other error: {err}')
+    return new_func
+
+
+# Получение списка параметров запроса из списка coins_list
+@error_catcher
+def get_query_params(list_: list) -> list:
+    return [f'currency_pair={currency_pair}' for currency_pair in list_]
+
+
+# Выполнение запроса и вывод результатов
+@error_catcher
+def get_data(query_params_list: list) -> None:
+    for query_param in query_params_list:
+        response = requests.get(f"{host}{prefix}{url}?{query_param}", headers=headers)
+
+        # Проверяем статус ответа
+        response.raise_for_status()
+
+        # Получаем название монеты из параметра запроса
+        currency_name = query_param.split('=')[1].split('_')[0]
+
+        # Получаем цены
+        ask = response.json()['asks'][0][0]
+        bid = response.json()['bids'][0][0]
+
+        print(f'Текущая цена продажи для {currency_name}: {ask}')
+        print(f'Текущая цена покупки для {currency_name}: {bid}')
+        print('----------')
+
+
+def main():
+    query_params_list = get_query_params(coins_list)
+    if query_params_list:  # Проверяем, что список не пустой
+        get_data(query_params_list)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+# 2 вариант попроще(изначальный)
+# import requests
+#
+# host = "https://api.gateio.ws"
+# prefix = "/api/v4"
+# headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+# url = '/spot/order_book'
+# coins_list = ['BTC_USDT', 'ETH_USDT', 'SOL_USDT']
+#
+# # В цикле выбираем торговую пару из списка и формируем url для запроса
+# for currency_pair in coins_list:
+#     try:
+#         query_param = f'currency_pair={currency_pair}'
+#         response = requests.get(f"{host}{prefix}{url}?{query_param}", headers=headers)
+#
+#         # Проверяем статус ответа
+#         response.raise_for_status()
+#
+#         # Вынесем в переменные название монеты, текущие бид и аск
+#         currency_name = currency_pair.split('_')[0]
+#         ask = response.json()['asks'][0][0]
+#         bid = response.json()['bids'][0][0]
+#
+#         print(f'Текущая цена продажи для {currency_name}: {ask}')
+#         print(f'Текущая цена покупки для {currency_name}: {bid}')
+#         print('----------')
+#     except requests.ConnectionError as err:
+#         print(f'Connection error occured: {err}')
+#     except requests.HTTPError as err:
+#         print(f'HTTP error occured: {err}')
+#     except Exception as err:
+#         print(f'Other error occured: {err}')
