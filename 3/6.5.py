0dd8e0a8e574a852c94c86bba0ebb7926763d0d5
ztknssl <davydov.andrey37@gmail.com>
2025-03-01 19:47:43 +0300

    Functions 2

--- 3/6.5.py
+++ 3/6.5.py
@@ -1,3 +1,6 @@
 def is_palindrome(string_: str):
-    string_ = string_.strip().replace(' ', '').casefold()
-    return string_ == string_[::-1]
+    try:
+        string_ = string_.strip().replace(' ', '').casefold()
+        return string_ == string_[::-1]
+    except AttributeError as e:
+        print(f'Нужно ввести строку: {e}')
