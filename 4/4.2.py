6b389642282a33c50b680de0a145eedf34b7e6a1
ztknssl <davydov.andrey37@gmail.com>
2025-03-20 00:38:06 +0300

    OOP 2 commit

---
+++ 4/4.2.py
@@ -0,0 +1,28 @@
+class Fruit:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+class Apple(Fruit):
+    @staticmethod
+    def taste():
+        print('Яблоко сладкое')
+
+class Banana(Fruit):
+    @staticmethod
+    def taste():
+        print('Банан мягкий')
+
+
+fruit = Fruit(name="Фрукт")
+print(fruit.get_name())
+
+apple = Apple(name="Яблоко")
+print(apple.get_name())
+apple.taste()
+
+banana = Banana(name="Банан")
+print(banana.get_name())
+banana.taste()
