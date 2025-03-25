3dee0f47c70804f373e5c93f57f1e454ea9c34cd
ztknssl <davydov.andrey37@gmail.com>
2025-03-26 02:23:43 +0300

    Asyncio №2

---
+++ 5/5.2.py
@@ -0,0 +1,38 @@
+import asyncio
+import random
+import time
+
+
+async def double(value: int) -> int:
+    sleep_time = random.randint(1, 3)
+    print(f'Спим {sleep_time}с, исходное число: {value}')
+    print('--------------')
+    await asyncio.sleep(sleep_time)
+    return value * 2
+
+
+async def main():
+    numbers = [1, 2, 3, 4, 5]
+
+    result = await asyncio.gather(*(double(item) for item in numbers), return_exceptions=True)
+    print(result)
+
+asyncio.run(main())
+
+
+# Опционально к 1 задаче
+
+# async def print_message(message: str) -> None:
+#     await asyncio.sleep(random.randint(1, 3))
+#     print(message)
+#
+#
+# async def main():
+#     list_ = ['Привет!', 'Как дела?', 'До свидания!']
+#
+#     # Уставнавливаем return_exceptions=True, чтобы не прерывать выполнение остальных задач, если
+#     # в какой-то возникнет исключение
+#
+#     await asyncio.gather(*(print_message(item) for item in list_), return_exceptions=True)#
+#
+# asyncio.run(main())
