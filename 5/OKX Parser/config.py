73b70caac8d4e8515755fd855c722f2773e5fd71
ztknssl <davydov.andrey37@gmail.com>
2025-03-29 17:42:35 +0300

    OKX Parser 4 commit. Added AsyncOKXParser.py

--- 5/OKX Parser/config.py
+++ 5/OKX Parser/config.py
@@ -1,3 +1,4 @@
 URL = 'https://www.okx.com/api/v5/market/book'
 TICKERS = ['BTC', 'ETH', 'BNB']
 FILENAME = 'okx.json'
+LOGS = 'logs.log'
--- 5/OKX Parser/logger.py
+++ 5/OKX Parser/logger.py
@@ -1,5 +1,6 @@
 from loguru import logger
+from config import LOGS
 
 
 # Настройка логгера с асинхронной очередью для вывода в консоль и запись в лог-файл
-logger.add("file.log", enqueue=True)
+logger.add(LOGS, enqueue=True)
